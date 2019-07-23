d3f29b471db3fc6592446be7c2ddac52df188211
julianferres <julianferres@gmail.com>
2019-07-23 17:07:36 -0300

    gcd, itermul y modexp

---
+++ Math/modexp.py
@@ -0,0 +1,13 @@
+def modexp( x, y, p ):
+	"""Exponenciacion logaritmica iterativa,
+	x^y (mod p), el orden el O(log y)"""
+
+	res = 1
+	while(y>0):
+		if(y & 1):
+			res*= x
+			res%=p
+		y >>= 1
+		x*= x
+
+	return res%p
