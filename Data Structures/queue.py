f91f96273a049de52a3e0e69e4f3cedb9a75ac25
julianferres <julianferres@gmail.com>
2019-08-01 22:28:44 -0300

    StackMin and QueueMin

--- Data Structures/queue.py
+++ Data Structures/queue.py
@@ -8,9 +8,9 @@ class Queue:
 	def __init__(self):
 		"""Create an empty queue"""
 		self.head = None
-		self.tail = None 
+		self.tail = None
 
-	
+
 	def isEmpty(self):
 		return self.head == None
 
@@ -18,25 +18,70 @@ class Queue:
 		"""Show queue peek"""
 		if(self.isEmpty()): raise ValueError("Peek has no elements.")
 		return self.head.data
-	
+
 	def enqueue(self,newData):
 		"""Enqueue an element to the queue"""
 
 		newNode = Node(newData)
-			
-		if(self.isEmpty()): 
+
+		if(self.isEmpty()):
 			self.head = newNode
 			self.tail = newNode
 		else:
 			self.tail.next = newNode
-	
+
 	def dequeue(self):
 		if(self.isEmpty()): raise ValueError("Empty queue, cannot dequeue")
-			
+
 		data = self.head.data
 		self.head = self.head.next
-		
-		if(self.head == None): self.tail = self.head		
-		
+
+		if(self.head == None): self.tail = self.head
+
 		return data
-		
+
+
+class QueueMin:
+
+	def __init__(self):
+		self.s1 = []
+		self.s2 = []
+
+	def min(self):
+		if(len(self.s1)==0 or len(self.s2)==0):
+			minimum = self.s2[-1][1] if(len(self.s1)==0) else self.s1[-1][1]
+		else:
+			minimum = min(self.s1[-1][1], self.s2[-1][1])
+		return minimum
+
+	def enqueue(self, new_element):
+		minimum = new_element if(len(self.s1)==0) else min(new_element,self.s1[-1][1])
+		self.s1.append((new_element, minimum))
+
+	def dequeue(self):
+		if(len(self.s2)==0):
+			while(not len(self.s1)==0):
+				element = self.s1[-1][0]
+				self.s1.pop()
+				minimum = element if(len(self.s2)==0) else min(element, self.s2[-1][1])
+				self.s2.append((element,minimum))
+
+		remove_element = self.s2[-1][0]
+		self.s2.pop()
+		return remove_element
+
+def minimumInSubarrays(a, k):
+	"""Recieve an array of length n and a positive length k
+	and returns minimun in all subarrays a[i:i+k] in O(n)"""
+	q = QueueMin()
+	out = []
+	for i in range(min(k,len(a))):
+		q.enqueue(a[i])
+	out.append(q.min())
+
+	for el in a[k:]:
+		q.enqueue(el)
+		q.dequeue()
+		out.append(q.min())
+
+	return out
--- Data Structures/stack.py
+++ Data Structures/stack.py
@@ -3,17 +3,49 @@ class Stack:
 	def __init__(self):
 		"""Create an empty stack"""
 		self.items = []
-	
+
 	def isEmpty(self):
 		return len(self.items)==0
 
 	def pop(self):
 		if(self.isEmpty()): raise ValueError("Empty stack, cannot pop")
-		
+
 		return self.items.pop()
-	
+
 	def push(self,newItem):
 		self.items.append(newItem)
-	
-		
-		
+
+class StackMin:
+
+    def __init__(self):
+        """Create an empty stack, with min Query in O(1):
+        contains elements (el,min_until_el)"""
+        self.items = []
+
+    def __str__(self):
+        return str(self.items)
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def pop(self):
+        if(self.isEmpty()):
+            raise ValueError("Empty stack, cannot pop.")
+
+        return self.items.pop()
+
+    def peek(self):
+        if(self.isEmpty()):
+            raise ValueError("Empty stack.")
+
+        return self.items[-1]
+
+    def push(self, newItem):
+        if(self.isEmpty()):
+            self.items.append((newItem,newItem))
+        else:
+            self.items.append((newItem, min(newItem, self.peek()[1])))
+
+    def min(self):
+        if(self.isEmpty()):
+            return None
+        return self.peek()[1]
