2c1b86e036188fa7364cfb442610f3b6b85f7956
julianferres <julianferres@gmail.com>
2019-09-25 13:05:52 -0300

    gitignore y plantilla

---
+++ plantilla.py
@@ -0,0 +1,15 @@
+I = lambda : int(input())
+LI = lambda : [int(x) for x in input().split()]
+MI = lambda : map(int, input().split())
+SI = lambda : input()
+
+"""
+#Leer de archivo
+for line in sys.stdin:
+    ...
+"""
+
+from collections import *
+from heapq import *
+from math import *
+import itertools
